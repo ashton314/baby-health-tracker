57af6ffcfdb24b625011645190fa9ade0b850cdd
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-07-12 15:48:52 -0600

    Add notes column, convenience functions

--- recorder.rkt
+++ recorder.rkt
@@ -2,7 +2,7 @@
 
 (require racket/pretty racket/match db)
 
-(struct record ([timestamp #:auto]) #:transparent #:auto-value "now")
+(struct record (notes [id #:auto] [timestamp #:auto]) #:transparent #:auto-value null)
 
 (struct diaper record (type) #:transparent)
 (struct breast-feed record (event side) #:transparent)
@@ -10,18 +10,18 @@
 
 (define (record-persist! rec)
   (match rec
-    [(diaper _ type) (query-exec (fetch-db-instance) "insert into diaper_log (type) values ($1)" type)]
-    [(breast-feed _ event side) (query-exec (fetch-db-instance) "insert into breastfeed_log (event, side) values ($1, $2)" event side)]
-    [(bottle-feed _ event quantity) (query-exec (fetch-db-instance) "insert into bottlefeed_log (event, quantity) values ($1, $2)" event quantity)]))
+    [(diaper notes _ _ type) (query-exec (fetch-db-instance) "insert into diaper_log (type, notes) values ($1, $2)" type notes)]
+    [(breast-feed notes _ _ event side) (query-exec (fetch-db-instance) "insert into breastfeed_log (event, side, notes) values ($1, $2, $3)" event side notes)]
+    [(bottle-feed notes _ _ event quantity) (query-exec (fetch-db-instance) "insert into bottlefeed_log (event, quantity, notes) values ($1, $2, $3)" event quantity notes)]))
 
 (define (init-db!)
   (let ([dbh (fetch-db-instance)])
     (query-exec dbh
-                "create table diaper_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, type TEXT)")
+                "create table if not exists diaper_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, type TEXT, notes TEXT)")
     (query-exec dbh
-                "create table breastfeed_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, event TEXT, side TEXT)")
+                "create table if not exists breastfeed_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, event TEXT, side TEXT, notes TEXT)")
     (query-exec dbh
-                "create table bottlefeed_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, event TEXT, quantity NUMERIC)")))
+                "create table if not exists bottlefeed_log (id integer primary key asc autoincrement, timestamp default CURRENT_TIMESTAMP, event TEXT, quantity NUMERIC, notes TEXT)")))
 
 (define *db-instance* null)
 (define (fetch-db-instance)
@@ -32,3 +32,11 @@
 (define (new-db-conn!)
   (displayln "Creating a new db connection" (current-error-port))
   (sqlite3-connect #:database "./baby.db" #:mode 'create))
+
+;;; Convenience functions
+(define (record-dirty! [notes ""]) (record-persist! (diaper notes "dirty")))
+(define (record-wet! [notes ""]) (record-persist! (diaper notes "wet")))
+(define (record-breastfeed-start! side [notes ""]) (record-persist! (breast-feed notes "start" side)))
+(define (record-breastfeed-stop! side [notes ""]) (record-persist! (breast-feed notes "stop" side)))
+(define (record-bottlefeed-start! quantity [notes ""]) (record-persist! (bottle-feed notes "start" quantity)))
+(define (record-bottlefeed-stop! quantity [notes ""]) (record-persist! (bottle-feed notes "stop" quantity)))
