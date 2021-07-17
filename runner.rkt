e58ed0f72ff015c6392e39540ce93c63f499dd29
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-07-16 21:21:19 -0600

    Add runner to make interacting with the tracker easier from the cli

---
+++ runner.rkt
@@ -0,0 +1,37 @@
+#!/usr/bin/env racket
+#lang racket
+
+(require "recorder.rkt")
+
+(define the-type (make-parameter "diaper"))
+(define the-subtype (make-parameter "wet"))
+(define ask-notes? (make-parameter #t))
+(define the-notes (make-parameter ""))
+
+(struct command (action type subtype notes) #:transparent)
+
+(define args
+  (command-line
+   #:once-each
+   [("-n" "--no-notes") "Do not record notes" (ask-notes? #f)]
+   [("-t" "--record-type") tp
+                           "Record type: must be either <diaper>, <breastfeed>, or <bottlefeed>"
+                           (the-type tp)]
+   [("-s" "--sub-type") st
+                        "Subtype: for type <diaper>, this should be <wet> or <dirty>; for feedings, should be <start> or <stop>"
+                        (the-subtype st)]
+   #:args ()
+   (command 'record (the-type) (the-subtype) "")))
+
+
+(when (ask-notes?)
+  (displayln "Notes:" (current-error-port))
+  (the-notes (port->string (current-input-port))))
+
+(match args
+  [(command 'record type subtype notes)
+   (match type
+     ["diaper" (match subtype ["wet" (record-wet (the-notes))] ["dirty" (record-dirty (the-notes))])]
+     ["breastfeed" (match subtype ["start" (record-breastfeed-start (the-notes))] ["stop" (record-breastfeed-stop (the-notes))])]
+     ["bottlefeed" (match subtype ["start" (record-bottlefeed-start (the-notes))] ["stop" (record-bottlefeed-stop (the-notes))])])])
+
